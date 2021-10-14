0f39c3fbb3f32dcd03ed16530ca207958d05b892
jamesbiswell <jamesbiswell@aol.com>
2021-10-14 12:50:49 +0100

    Section 9.167 Assignment - Gotchas with Project Lombok

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -1,11 +1,13 @@
 package guru.springframework.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 public class Notes {
 
     @Id
